b540b3ce9b6261a1202cba06d4c7bd20ef2b4220
smagdela <smagdela@student.42.fr>
2022-06-13 19:20:50 +0200

    ex04 comiling but inifnite loop

--- module_01/ex04/sources/main.cpp
+++ module_01/ex04/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 11:39:51 by smagdela          #+#    #+#             */
-/*   Updated: 2022/06/13 17:22:54 by smagdela         ###   ########.fr       */
+/*   Updated: 2022/06/13 19:19:53 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <string>
 
 bool	gnl(std::ifstream &file, std::string &line)
 {
@@ -31,18 +32,32 @@ void	winner_sed(std::ifstream &file, std::ofstream &replace_file,
 	std::string const sep, std::string const rep)
 {
 	std::string	line;
+	size_t		pos = 0;
+	size_t		start = 0;
 
 	while (gnl(file, line))
 	{
-		if (line.find/* etc... */)
+		pos = 0;
+		while (pos != std::string::npos)
+		{
+			start = pos;
+			pos = line.find(sep, pos);
+			if (pos != std::string::npos)
+			{
+				replace_file << line.substr(start, pos - start) + rep;
+				pos += sep.size();
+			}
+			else
+				replace_file << std::endl;
+		}
 	}
 }
-	
 
 int	main(int ac, char **av)
 {
 	std::ifstream	file;
 	std::ofstream	replace_file;
+	std::string		replace_filename;
 
 	if (ac != 4)
 	{
@@ -52,7 +67,8 @@ int	main(int ac, char **av)
 	file.open(av[1]);
 	if (!file.is_open())
 		return (EXIT_FAILURE);
-	replace_file.open(av[1] + std::string(".replace"), std::ios::app);
+	replace_filename = av[1] + std::string(".replace");
+	replace_file.open(replace_filename.c_str(), std::ios::app);
 	if (!replace_file.is_open())
 	{
 		file.close();
