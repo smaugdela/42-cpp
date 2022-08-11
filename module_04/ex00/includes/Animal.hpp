63a60efb4aa03c7d8422d5e3384035d7ef48e5f5
smagdela <smagdela@student.42.fr>
2022-08-11 15:53:00 +0200

    Module 04 ex00 WIP

---
+++ module_04/ex00/includes/Animal.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 15:51:56 by smagdela          #+#    #+#             */
+/*   Updated: 2022/08/11 15:51:59 by smagdela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+# include <string>
+
+class Animal
+{
+
+	public:
+
+		Animal();
+		Animal( Animal const & src );
+		~Animal();
+
+		Animal &		operator=( Animal const & rhs );
+
+	private:
+
+};
+
+std::ostream &			operator<<( std::ostream & o, Animal const & i );
+
+#endif /* ********************************************************** ANIMAL_H */
