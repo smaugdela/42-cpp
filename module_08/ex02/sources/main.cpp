b538244a25aeeabe78434ae2e3530dfffe79c557
smagdela <smagdela@student.42.fr>
2022-09-26 12:21:12 +0200

    module_08 ex02 done!

--- module_08/ex02/sources/main.cpp
+++ module_08/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 12:40:20 by smagdela          #+#    #+#             */
-/*   Updated: 2022/09/23 16:31:27 by smagdela         ###   ########.fr       */
+/*   Updated: 2022/09/26 12:19:10 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,27 @@
 
 int main()
 {
-	std::cout << "\033[0;34m\033[1m\n\t Test with Mutantstack \033[0m\n" << std::endl;
+	std::cout << "\033[0;34m\033[1m\n\t Testing basic MutantStack methods. \033[0m\n" << std::endl;
 
 	MutantStack<int> mstack;
 
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "mstack.top() = " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "mstack.size() after pop() = " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
 
+	std::cout << "\033[0;34m\033[1m\n\t Printing MutantStack and testing iterators. \033[0m\n" << std::endl;
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
@@ -41,30 +44,23 @@ int main()
 	}
 	std::stack<int> s(mstack);
 
-	std::cout << "\033[0;34m\033[1m\n\t Test with list. The exact same results should be found if correct. \033[0m\n" << std::endl;
+	std::cout << "\033[0;34m\033[1m\n\t Test with list. It should be filled with the same ints as MutantStack. \033[0m\n" << std::endl;
 
 	std::list<int> lst;
 
-	lst.push_front(5);
-	lst.push_front(17);
-	std::cout << lst.front() << std::endl;
-	lst.pop_front();
-	std::cout << lst.size() << std::endl;
-	lst.push_front(3);
-	lst.push_front(5);
-	lst.push_front(737);
-	lst.push_front(0);
+	lst.push_back(5);
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
 
 	std::list<int>::iterator itl = lst.begin();
 	std::list<int>::iterator itel = lst.end();
-	++itl;
-	--itl;
 	while (itl != itel)
 	{
 		std::cout << *itl << std::endl;
 		++itl;
 	}
-	std::list<int> sl(lst);
 
 	return EXIT_SUCCESS;
 }
